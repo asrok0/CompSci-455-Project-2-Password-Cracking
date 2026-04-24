97d706fb610f91369516ac5a2526b8933099f017
asrok0 <akiraakia.04@gmail.com>
2026-04-24 09:03:57 -0500

    Most of the main class for testing/setup

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,47 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        // Stolen hashes
+        String target1Hash = "a1337405aa016df43663b2853bffc091";
+        String target2Hash = "eb20c9abc2e597b1e03c6200cfe1de08";
+
+        RainbowTable rainbowTable = new RainbowTable();
+        int totalPasswords = 0;
+        long startTime = System.currentTimeMillis();
+
+        // Generate possible passwords for first target
+        List<String> target1Passwords = PasswordGenerator.generateTarget1Passwords();
+
+        for (String password : target1Passwords) {
+            rainbowTable.addPassword(password);
+            totalPasswords++;
+        }
+
+        // Generate possible passwords for second target
+        List<String> target2Passwords = PasswordGenerator.generateTarget2Passwords();
+
+        for (String password : target2Passwords) {
+            rainbowTable.addPassword(password);
+            totalPasswords++;
+        }
+
+        // Get CSV version of rainbow table
+        rainbowTable.exportCSV("rainbow_table.csv");
+
+        // Crack hashes
+        String target1Cracked = rainbowTable.crackHash(target1Hash);
+        String target2Cracked = rainbowTable.crackHash(target2Hash);
+
+        long endTime = System.currentTimeMillis();
+
+        // Results
+        System.out.println("--------------------");
+        System.out.println("RESULTS");
+        System.out.println("--------------------");
+
+        //TODO: Add printing for cracked results
+
+        System.out.println("\n\nPasswords Tested: " + totalPasswords);
+        System.out.println("Runtime: " + (endTime - startTime) + "ms");
     }
 }
