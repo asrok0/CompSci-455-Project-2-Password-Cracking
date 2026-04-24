af1942d8bb052ade78b1e4542c4a28c096d8a12b
asrok0 <akiraakia.04@gmail.com>
2026-04-24 08:53:41 -0500

    MD5 work

--- src/MD5Util.java
+++ src/MD5Util.java
@@ -6,5 +6,18 @@ public class MD5Util {
     // Converts plaintext to hash
     public static String md5(String input) {
 
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes("ASCII"));
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error generating hash", e);
+        }
     }
 }
